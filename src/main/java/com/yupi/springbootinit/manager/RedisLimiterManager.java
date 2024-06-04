19ab294486b82e4c9421cbd9d880ba8db2e54b73
oldmomsimith <oldmomsimith@gmail.com>
2024-06-04 11:38:06 +0800

    redisson的配置完成

---
+++ src/main/java/com/yupi/springbootinit/manager/RedisLimiterManager.java
@@ -0,0 +1,28 @@
+package com.yupi.springbootinit.manager;
+
+import com.yupi.springbootinit.common.ErrorCode;
+import com.yupi.springbootinit.exception.BusinessException;
+import org.redisson.api.RRateLimiter;
+import org.redisson.api.RateIntervalUnit;
+import org.redisson.api.RateType;
+import org.redisson.api.RedissonClient;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+
+@Service
+public class RedisLimiterManager {
+
+    @Resource
+    private RedissonClient redissonClient;
+
+    public void doRateLimit(String key) {
+        RRateLimiter rateLimiter = redissonClient.getRateLimiter(key);
+        rateLimiter.trySetRate(RateType.OVERALL, 2, 1, RateIntervalUnit.SECONDS);
+
+        boolean canOp = rateLimiter.tryAcquire(1);
+        if(!canOp) {
+            throw new BusinessException(ErrorCode.TOO_MANY_REQUEST);
+        }
+    }
+}
